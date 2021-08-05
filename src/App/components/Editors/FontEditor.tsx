11c1e4dade0353df111143072a272c0586d3c424
Joel <9orangetigers@gmail.com>
2021-08-06 11:02:42 +1200

    Last few file changes

--- src/App/components/Editors/FontEditor.tsx
+++ src/App/components/Editors/FontEditor.tsx
@@ -13,7 +13,6 @@ const FONT_SIZES = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((n) => ({
   label: `${n}pt`,
 }));
 
-
 interface HTMLSelectValueElement extends HTMLSelectElement {
   value: any;
 }
