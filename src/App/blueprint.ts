3ee1765c3cdab252b542cc3c251d99f275b696bf
Joel <9orangetigers@gmail.com>
2021-08-06 12:56:16 +1200

    Add in the extra changes to blueprint.ts

--- src/App/blueprint.ts
+++ src/App/blueprint.ts
@@ -1,5 +1,5 @@
 import * as BP from '@blueprintjs/core';
-import styled from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 import { space, position, flexbox, typography, layout, SpaceProps, PositionProps, FlexboxProps, TypographyProps, LayoutProps } from 'styled-system';
 
 type STLProps = SpaceProps & TypographyProps & LayoutProps;
@@ -10,49 +10,49 @@ type SLPProps = SpaceProps & LayoutProps & PositionProps;
 
 type SLFPProps = SpaceProps & LayoutProps & FlexboxProps & PositionProps;
 
-export const Text = styled(BP.Text)<STLProps>`
+export const Text: StyledComponent<typeof BP.Text, any, {} & STLProps> = styled(BP.Text)`
   ${space} ${typography} ${layout}
 `;
-export const H1 = styled(BP.H1)<STLProps>`
+export const H1: StyledComponent<typeof BP.H1, any, {} & STLProps> = styled(BP.H1)`
   ${space} ${typography} ${layout}
 `;
-export const H2 = styled(BP.H2)<STLProps>`
+export const H2: StyledComponent<typeof BP.H2, any, {} & STLProps> = styled(BP.H2)`
   ${space} ${typography} ${layout}
 `;
-export const H3 = styled(BP.H3)<STLProps>`
+export const H3: StyledComponent<typeof BP.H3, any, {} & STLProps> = styled(BP.H3)`
   ${space} ${typography} ${layout}
 `;
-export const H4 = styled(BP.H4)<STLProps>`
+export const H4: StyledComponent<typeof BP.H4, any, {} & STLProps> = styled(BP.H4)`
   ${space} ${typography} ${layout}
 `;
-export const H5 = styled(BP.H5)<STLProps>`
+export const H5: StyledComponent<typeof BP.H5, any, {} & STLProps> = styled(BP.H5)`
   ${space} ${typography} ${layout}
 `;
-export const H6 = styled(BP.H6)<STLProps>`
+export const H6: StyledComponent<typeof BP.H6, any, {} & STLProps> = styled(BP.H6)`
   ${space} ${typography} ${layout}
 `;
-export const Button = styled(BP.Button)<SLPProps>`
+export const Button: StyledComponent<typeof BP.Button, any, {} & SLPProps> = styled(BP.Button)`
   ${space} ${layout} ${position}
 `;
-export const ButtonGroup = styled(BP.ButtonGroup)<SLPProps>`
+export const ButtonGroup: StyledComponent<typeof BP.ButtonGroup, any, {} & SLPProps> = styled(BP.ButtonGroup)`
   ${space} ${layout} ${position}
 `;
-export const Icon = styled(BP.Icon)<SLPProps>`
+export const Icon: StyledComponent<typeof BP.Icon, any, {} & SLPProps> = styled(BP.Icon)`
   ${space} ${layout} ${position}
 `;
-export const Card = styled(BP.Card)<SLFProps>`
+export const Card: StyledComponent<typeof BP.Card, any, {} & SLFPProps> = styled(BP.Card)`
   ${space} ${layout} ${flexbox}
 `;
-export const Checkbox = styled(BP.Checkbox)<SLFProps>`
+export const Checkbox: StyledComponent<typeof BP.Checkbox, any, {} & SLFPProps> = styled(BP.Checkbox)`
   ${space} ${layout} ${flexbox}
 `;
-export const Radio = styled(BP.Radio)<SLFProps>`
+export const Radio: StyledComponent<typeof BP.Radio, any, {} & SLFPProps> = styled(BP.Radio)`
   ${space} ${layout} ${flexbox}
 `;
-export const HTMLSelect = styled(BP.HTMLSelect)<SLFProps>`
+export const HTMLSelect: StyledComponent<typeof BP.HTMLSelect, any, {} & SLFPProps> = styled(BP.HTMLSelect)`
   ${space} ${layout} ${flexbox}
 `;
-export const Tag = styled(BP.Tag)<SLFPProps>`
+export const Tag: StyledComponent<typeof BP.Tag, any, {} & SLFPProps> = styled(BP.Tag)<SLFPProps>`
   ${space} ${layout} ${flexbox} ${position}
 `;
 
