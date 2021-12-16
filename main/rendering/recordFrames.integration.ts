6800f6f4e79adc94a15fd5d2d4652b5094d6a7f3
Ira Hopkinson <ira_hopkinson@sil.org>
2021-12-16 12:40:18 +0700

    - change require to import

--- main/rendering/recordFrames.integration.ts
+++ main/rendering/recordFrames.integration.ts
@@ -2,13 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import test from 'ava';
 import pixelmatch from 'pixelmatch';
+import { PNG } from 'pngjs';
 import tmp from 'tmp-promise';
 import { AnimationSettings } from '../../src/models/animationSettings.model';
 import { Timings } from '../models/timings.model';
 import { record } from './recordFrames';
 import { getHtml } from './renderFrames';
-const PNG = require('pngjs').PNG;
-
 
 test('recordFrames: render 5 mock frames', async (t) => {
   const htmlContent = createMockHtml();
@@ -42,18 +41,18 @@ test('recordFrames: verify frames are not all the same', async (t) => {
 
   const diffPixels = [];
   // loop through generated images and stop at every 15th (1 second)
-  for(let i=1; i < directoryOfFrameFiles.length; i = i + 15) {
+  for (let i = 1; i < directoryOfFrameFiles.length; i = i + 15) {
     // generate compare image names
-    const imagePad1 = (i).toString().padStart(6, '0');
-    const imagePad2 = (i+1).toString().padStart(6, '0');
+    const imagePad1 = i.toString().padStart(6, '0');
+    const imagePad2 = (i + 1).toString().padStart(6, '0');
     // read compare images
-    const img1 = PNG.sync.read(fs.readFileSync(path.join(outputLocation,'frame_'+imagePad1+'.png')));
-    const img2 = PNG.sync.read(fs.readFileSync(path.join(outputLocation,'frame_'+imagePad2+'.png')));
+    const img1 = PNG.sync.read(fs.readFileSync(path.join(outputLocation, 'frame_' + imagePad1 + '.png')));
+    const img2 = PNG.sync.read(fs.readFileSync(path.join(outputLocation, 'frame_' + imagePad2 + '.png')));
     // get the image dimensions and create a placeholder PNG
-    const {width, height} = img1;
-    const diff = new PNG({width, height});
+    const { width, height } = img1;
+    const diff = new PNG({ width, height });
     // for each set of images use pixelmatch to find the number of different pixels
-    diffPixels.push(pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0}));
+    diffPixels.push(pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0 }));
     // TIP: if you would like to see a PNG that shows the differences of the images uncomment this line
     // fs.writeFileSync('diff_'+i+'.png', PNG.sync.write(diff));
   }
@@ -137,7 +136,7 @@ function mockTimings(): Timings {
         { word: 'is', start: 1001, end: 2000 },
         { word: 'just', start: 2001, end: 3000 },
         { word: 'a', start: 3001, end: 4000 },
-        { word: 'test.', start: 4001, end: 5000 }
+        { word: 'test.', start: 4001, end: 5000 },
       ],
     },
   ];
